5b376464ddbcb26907bc197585e5544bad141021
jedeka <jedekaa@gmail.com>
2026-03-31 16:49:01 +0800

    updated video_utils.py for torchvision VideoReader.io problem

--- setup.sh
+++ setup.sh
@@ -2,8 +2,10 @@
 
 # usage: bash setup.sh <LEROBOT ROOT DIR>
 POLICIES_DIR="${1}/src/lerobot/policies/"
+POLICIES_DIR="${1}/src/lerobot/datasets/"
 echo "Copying to ${POLICIES_DIR}"
 
 cp factory.py ${POLICIES_DIR}/
+cp video_utils.py ${DATASETS_DIR}/
 cp -r mmact ${POLICIES_DIR}/
 cp mmact.sh ../
